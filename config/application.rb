b49db1a2cd67abecef1bbeb1431327b6f8ea2240
Tim Mecklem <timothy@mecklem.com>
2023-05-18 09:24:36 -0400

    `bundle add good_job` `bin/rails g good_job:install` `bin/rails db:migrate`
    Configure the adapter:
    config.active_job.queue_adapter = :good_job

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module GoodJobLab
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.active_job.queue_adapter = :good_job
 
     # Configuration for the application, engines, and railties goes here.
     #
