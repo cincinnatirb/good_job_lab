ab0218dbe97d794d5a9e69d36658bb7f8da87a40
Tim Mecklem <timothy@mecklem.com>
2023-06-15 11:30:50 -0400

    Add schedule for running the pi calculation job

--- config/schedule.rb
+++ config/schedule.rb
@@ -16,5 +16,9 @@
 # every 4.days do
 #   runner "AnotherModel.prune_old_records"
 # end
+#
+every 1.minute do
+  runner "PiCalculationJob.perform_later"
+end
 
 # Learn more: http://github.com/javan/whenever
